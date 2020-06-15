74bb24cddc6fb2ce9bc70d98375fac849d85d989
lyc <1217563553@qq.com>
2020-06-15 15:29:51 +0800

    add cal varsEffect on Ts function and optimize some filename

---
+++ p2_processCMIP6Data/s99.plot/s2_plot_nonLocalCld_dTsTrend.m
@@ -0,0 +1,9 @@
+%%---------------------------------------------------------
+% Author       : LYC
+% Date         : 2020-06-15 13:43:44
+% LastEditTime : 2020-06-15 13:43:45
+% LastEditors  : LYC
+% Description  : 
+% FilePath     : /Research/p2_processCMIP6Data/s99.plot/s2_plot_toadTsTrend_colorbar12.m
+%  
+%%---------------------------------------------------------
